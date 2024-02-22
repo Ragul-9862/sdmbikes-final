13a2b73f504c7c64fd81d4b860c40f37a1af95ed
Ragul ragul <ragul@clouddreams.in>
2024-02-22 17:41:15 +0530

    Re-upload of git

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,17 +1,7 @@
-import React from 'react'
+import React  from 'react'
 
 export default function Loading() {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-     
-      const timeout = setTimeout(() => {
-        setLoading(false);
-      }, 3000); 
-  
-      
-      return () => clearTimeout(timeout);
-    }, []); 
+   
   return (
     <div className="loading">
    <img src={require("../Components/assets/Navbar/loader.gif")}/>
