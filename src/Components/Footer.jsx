92dece3892dd02833afbd93ee2eac7689ffbf8cb
Ragul ragul <ragul@clouddreams.in>
2024-04-03 14:37:19 +0530

    Address changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -43,7 +43,7 @@ export default function Footer() {
       </div>
       <div className="col-lg-4">
         <div className="map-location">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3915.938054602987!2d76.98281827504579!3d11.043271289122158!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTHCsDAyJzM1LjgiTiA3NsKwNTknMDcuNCJF!5e0!3m2!1sen!2sin!4v1711711081544!5m2!1sen!2sin" width="100%" height={250} style={{border: 0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3915.927508513706!2d76.9851343!3d11.044061899999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba859007d08a1f1%3A0xdac7091643c0e8e8!2sSDM%20Bikes%20(Jawa%20Yezdi%20Motorcycles)!5e0!3m2!1sen!2sin!4v1712134708694!5m2!1sen!2sin" width="100%" height={250} style={{border: 0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
         </div>
       </div>
     </div>
